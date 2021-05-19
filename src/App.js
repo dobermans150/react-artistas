77dfa2832d9df16e8cf9707a356e861e46e90168
Christian Guevara <christianjguevara@hotmail.com>
2021-05-19 18:12:23 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, Fragment } from "react";
+import axios from "axios";
+import Formulario from "./components/Formulario";
+import Cancion from "./components/Cancion";
+import Informacion from "./components/Informacion";
 
 function App() {
+  //Utilizar usestate con 3 states diferentes
+
+  const [artista, agregarArtista] = useState("");
+  const [letra, agregarLetra] = useState([]);
+  const [info, agregarInfo] = useState({});
+
+  /* Metodo para consultar la API de Letras de canciones */
+
+  const consultarAPILetra = async busqueda => {
+    const { artista, cancion } = busqueda;
+    const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+
+    /* consultar a la api */
+    const resultado = await axios(url);
+
+    /* almacenar el artista que se busco */
+
+    agregarArtista(artista);
+    /*  almacenar la letra en el state */
+
+    agregarLetra(resultado.data.lyrics);
+  };
+
+  /* Metodo para consultar la API de Informacion */
+
+  const consultarAPIInfo = async () => {
+    if (artista) {
+      const url = `https://theaudiodb.com/api/v1/json/1/search.php?s=${artista}`;
+      const resultado = await axios(url);
+
+      agregarInfo(resultado.data.artists[0]);
+      console.log(resultado);
+    }
+  };
+
+  useEffect(() => {
+    consultarAPIInfo();
+  }, [artista]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Formulario consultarAPILetra={consultarAPILetra} />
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6">
+            <Informacion info={info} />
+          </div>
+          <div className="col-md-6">
+            <Cancion letra={letra} />
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
